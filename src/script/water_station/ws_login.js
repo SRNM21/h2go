bd4d85d63a7b012f5b390b36d39071d1dc2e1465
SRNM21 <gregorio1nonly@gmail.com>
2025-04-30 23:25:24 +0800

    refactor code

--- src/script/water_station/ws_login.js
+++ src/script/water_station/ws_login.js
@@ -4,7 +4,13 @@ import {
     validateRequired
 } from '../../util/util.js'
 
-const bcrypt = dcodeIO.bcrypt;
+var WSACC = []
+preLoadAccounts()
+
+const toastContainer = $('#toast-container')
+const loadingModal = $('#loading-modal')
+
+const bcrypt = dcodeIO.bcrypt
 const signInForm = $('#sign-in-form')
 
 const emailInput = $('#email')
@@ -12,66 +18,109 @@ const emailFeedback = $('#email-invalid-fb')
 const passwordInput = $('#password')
 const passwordFeedback = $('#password-invalid-fb') 
 
-const hashedPassword = '$2b$12$34HTmGupF7PNHHnhFzK15u08Hm86a.hXUONyyhyQVDjVjAVpyhcci';
-const plainPassword = "password";
-
-bcrypt.compare(plainPassword, hashedPassword, function (err, result) {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    if (result) {
-        console.log("Password is correct!");
-    } else {
-        console.log("Password is incorrect!");
-    }
-});
-
-signInForm.on("submit", function() 
+signInForm.on("submit", function(e) 
 {
     // ? Validate each input first and add feedback before final checking of form validation
-    const isPasswordValid = validateRequired(passwordInput, passwordFeedback);
-    const isEmailValid = validateEmail(emailInput, emailFeedback);
+    const isPasswordValid = validateRequired(passwordInput, passwordFeedback)
+    const isEmailValid = validateEmail(emailInput, emailFeedback)
 
     const areAllInputsValid = isPasswordValid && isEmailValid
 
     if (areAllInputsValid)
     {
-        console.log('LOGIN');
-        
-
+        loadingModal.modal('show')
+        loadingModal.one('shown.bs.modal', queryAccount)
     }
-
+    
+    e.preventDefault()
 })
 
-function attemptLogin()
+function preLoadAccounts()
 {
-    let curr = $(e.currentTarget)
-    
-    let messageID = curr.data('message-id')
-    let customerName = curr.data('customer-name')
-    let img = curr.find('.contact-profile').attr('src')
-
+    console.log('loading')
     $.ajax({
         url: '../../../../data/client/h2go_clients.xml',
         dataType: 'xml',
-        success: function(xml)
-        {
-            xmlData = $(xml)
-            loadContactMessages(messageID, customerName, img)
-        },
-        error: function(xhr, status, error) 
-        {
-            let errorPage = $(`
-                <div class="card error-page">
-                    <div class="card-body">
-                        <h5 class="card-title">Error Occured:</h5>
-                        <p class="card-text">Invalid order details (${error} | ${status}).</p>
-                    </div>
-                </div>
-            `)
+        success: (xml) => {
 
-            $('main').append(errorPage)
+            $(xml).find('water-stations water-station').each(function () {
+                const $station = $(this)
+
+                WSACC.push({
+                    xml: $station,
+                    email: $station.find('account-details email').text(),
+                    password: $station.find('account-details password').text(),
+                })
+            })
+            
+        },
+        error: (xhr, status, error) => {
+            console.log(error)
+            loadingModal.modal('hide')
+
+            // TODO ADD ERROR PAGE
         }
     })
+}
+
+function queryAccount(xml) { 
+    const email = emailInput.val()
+    const password = passwordInput.val()
+
+    const account = WSACC.find(acc => acc.email === email)
+
+    console.log(account);
+
+    if (!account) 
+    {
+        notify('danger', 'Account does not exist!')
+        loadingModal.modal('hide')
+        return
+    }
+    
+
+    bcrypt.compare(password, account.password, function (err, result) {
+        if (err) 
+            {
+            notify('danger', err)
+            return
+        }
+
+        if (result) 
+        {
+            console.log("Password is correct!")
+            notify('success', 'Password is correct!')
+        } 
+        else 
+        {
+            console.log("Password is incorrect!")
+            notify('danger', 'Password is incorrect!')
+        }
+
+
+        loadingModal.modal('hide')
+    })
+
+}
+
+function notify(type, content)
+{
+    let toast = $(`
+        <div class='toast toast-${type} align-items-center show' role='alert' aria-live='assertive' aria-atomic='true'>
+            <div class='d-flex'>
+                <div class='toast-body'>
+                    ${content}
+                </div>
+                <button type='button' class='btn-close me-2 m-auto' data-bs-dismiss='toast' aria-label='Close'></button>
+            </div>
+        </div>    
+    `)
+
+    toastContainer.append(toast)
+
+    setTimeout(function() {
+        toast.fadeOut('slow', function() {
+            $(this).remove()
+        })
+    }, 5000)
 }
